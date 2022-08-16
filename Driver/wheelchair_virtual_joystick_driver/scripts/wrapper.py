3c9afe5bf6c661f8bae52d22fefa4ac1ab3ac3ec
Lewis  Brand <lewisbrand.56@gmail.com>
2022-08-16 15:59:58 +0100

    Added basic joystick publisher to the wrapper

--- Driver/wheelchair_virtual_joystick_driver/scripts/wrapper.py
+++ Driver/wheelchair_virtual_joystick_driver/scripts/wrapper.py
@@ -2,6 +2,8 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+import std_msgs.msg
 from driver import Joystick
 
 class Wheelchair_virtual_joystick_driver:
@@ -32,6 +34,7 @@ class Wheelchair_virtual_joystick_driver:
         self.stop()
 
         rospy.Subscriber('/cmd_vel', Twist, self.on_twist)
+        self.joy_pub = rospy.Publisher('/joy', Joy, queue_size=10)
 
     def stop(self):
         """Called when the wrapper is stopping.
@@ -62,10 +65,24 @@ class Wheelchair_virtual_joystick_driver:
             rospy.loginfo("Trimming z to -100")
             z = -100
 
-
         rospy.loginfo("X = {}, Z = {}".format(x, z))
         self.joystick.set_percent(x,z)
 
+    def publish_joy(self):
+        rate = rospy.Rate(10) # 10hz
+
+        while not rospy.is_shutdown():
+            x, z = self.joystick.get_percent()
+            h = std_msgs.msg.Header()
+            h.stamp = rospy.Time.now()
+            
+            joy = Joy()
+            joy.header = h
+            joy.axes = [x,z]
+            joy.buttons = []
+
+            self.joy_pub.publish(joy)
+            rate.sleep()
 
 if __name__ == "__main__":
     rospy.init_node("wheelchair_virtual_joystick_driver")
@@ -74,4 +91,10 @@ if __name__ == "__main__":
     rospy.on_shutdown(wrapper.stop)
 
     rospy.loginfo("Joystick driver running. Controller can be turned on")
+
+    try:
+        wrapper.publish_joy()
+    except rospy.ROSInterruptException:
+        pass
+
     rospy.spin()
